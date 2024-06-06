72f0294c4fe2fd14b880d4baf6502cb4f1a8e699
Harshit Chaturvedi <162025255+HarshitChaturvedii@users.noreply.github.com>
2024-06-06 23:04:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var i = Math.random();
+i = Math.floor(i*6) + 1;
+
+
+var j = Math.random();
+j = Math.floor(j*6) + 1;
+ 
+var head
+if(i>j){
+    head = "<h1>🚩</h1><h1>Player 1 wins</h1>";
+}
+else if(j>i){
+     head = "<h1>Player 2 wins</h1><h1>🚩</h1 id='hh' >";
+}
+else{
+    head = "<h1>Draw</h1>";
+}
+var img1 = "<img src =" + "'./images/dice" + i + ".png'>" ;
+var img2 = "<img src =" + "'./images/dice" + j + ".png'>" ;
+
+document.querySelector(".theImages1").innerHTML = (img1);
+document.querySelector(".theImages2").innerHTML = (img2);
+document.querySelector(".header").innerHTML = (head);
+
